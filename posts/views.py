b3ed5e051afe7af810b18aa80176561510f31518
robyncam <robyncameron7@gmail.com>
2022-07-05 16:00:59 -0600

    created a posts url and function and html page so that when you click on a post you will be taken to that page and you can view all of the body

--- posts/views.py
+++ posts/views.py
@@ -7,3 +7,8 @@ def index(request):
     posts = Post.objects.all()
     #get all of the objects from the database Posts
     return render(request, 'index.html', {'posts': posts})
+
+
+def post(request, pk):
+    posts = Post.objects.get(id=pk)
+    return render(request, 'posts.html', {'posts': posts})
