19f70561f6048833357101fbd6c4815078b669c3
robyncam <robyncameron7@gmail.com>
2022-07-05 15:28:37 -0600

    created a model called Post which handles our blog posts

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from posts.models import Post
 
 # Create your views here.
+
+def index(request):
+    posts = Post.objects.all()
+    #get all of the objects from the database Posts
+    return render(request, 'index.html', {'posts': posts})
